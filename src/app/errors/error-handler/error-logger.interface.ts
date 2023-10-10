9984e737bb529ca3437ead9d85b37a22a2dab4cc
PsevdoKot <youkos00@mail.ru>
2023-10-10 14:17:32 +0500

    Release

--- src/app/errors/error-handler/error-logger.interface.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export interface IErrorLogger {
-  log(error: Error): void;
-}
-
-export const ERROR_LOGGER: InjectionToken<string> =
-  new InjectionToken<IErrorLogger>('ERROR_LOGGER');
--- src/app/statistics/dto/review.dto.ts
+++
@@ -1,7 +0,0 @@
-import { UserDto } from './user.dto';
-
-export class ReviewDto {
-  public user: UserDto;
-  public state: string;
-  public created_at: string;
-}
